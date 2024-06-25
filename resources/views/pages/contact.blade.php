d8a112f3f635acfb3c88f31558376324901116d8
kutumb_app <lakshmikantpandey02@gmail.com>
2024-06-25 19:00:33 +0530

    models added for rates and models

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -11,6 +11,78 @@
     </div>
 
   </div>
+  <div class="col-xl-12 col-md-12 d-flex" data-aos="zoom-in" data-aos-delay="200">
+    <div class="member-img">
+      <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
+    </div>
+  </div>
+
+  <div class="d-flex align-items-center mt-4">
+    <i class="bi bi-check2"></i>
+    <h4>Save more money by booking high class profile girls in our escorts service</h4>
+  </div>
+  <p>Unlock exceptional savings by booking our high-class profile girls. Experience luxury without the premium price tag. Discover elegance and sophistication at affordable rates. Reserve your companion today and indulge in a memorable experience that doesn't break the bank.</p>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-3">
+        <div class="card">
+          <img class="card-img-top" src="assets/img/testimonials/testimonials-2.jpg" alt="Card image cap">
+          <div class="card-body text-center">
+            <h5 class="card-title">College Escorts</h5>
+            <p class="card-text">1 hour - Rs 9599 - Rs 8000</p>
+            <p class="card-text">2 hour - Rs 10599 - Rs 8999</p>
+            <p class="card-text">6 hour - Rs 17000 - Rs 13000</p>
+            <p class="card-text">Dinner Date - Rs 7999</p>
+            <p class="card-text">Full Night/Day - Rs 26000</p>
+            <a href="#" class="btn btn-info">Book</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="card">
+          <img class="card-img-top" src="assets/img/testimonials/testimonials-2.jpg" alt="Card image cap">
+          <div class="card-body text-center">
+            <h5 class="card-title">College Escorts</h5>
+            <p class="card-text">1 hour - Rs 9599 - Rs 8000</p>
+            <p class="card-text">2 hour - Rs 10599 - Rs 8999</p>
+            <p class="card-text">6 hour - Rs 17000 - Rs 13000</p>
+            <p class="card-text">Dinner Date - Rs 7999</p>
+            <p class="card-text">Full Night/Day - Rs 26000</p>
+            <a href="#" class="btn btn-info">Book</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="card">
+          <img class="card-img-top" src="assets/img/testimonials/testimonials-2.jpg" alt="Card image cap">
+          <div class="card-body text-center">
+            <h5 class="card-title">College Escorts</h5>
+            <p class="card-text">1 hour - Rs 9599 - Rs 8000</p>
+            <p class="card-text">2 hour - Rs 10599 - Rs 8999</p>
+            <p class="card-text">6 hour - Rs 17000 - Rs 13000</p>
+            <p class="card-text">Dinner Date - Rs 7999</p>
+            <p class="card-text">Full Night/Day - Rs 26000</p>
+            <a href="#" class="btn btn-info">Book</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="card">
+          <img class="card-img-top" src="assets/img/testimonials/testimonials-2.jpg" alt="Card image cap">
+          <div class="card-body text-center">
+            <h5 class="card-title">College Escorts</h5>
+            <p class="card-text">1 hour - Rs 9599 - Rs 8000</p>
+            <p class="card-text">2 hour - Rs 10599 - Rs 8999</p>
+            <p class="card-text">6 hour - Rs 17000 - Rs 13000</p>
+            <p class="card-text">Dinner Date - Rs 7999</p>
+            <p class="card-text">Full Night/Day - Rs 26000</p>
+            <a href="#" class="btn btn-info">Book</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <div class="map">
     <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
@@ -23,7 +95,7 @@
       <div class="col-lg-4">
 
         <div class="info">
-          <h3>Get in touch</h3>
+          <h3>Contact us for booking</h3>
           <p>Et id eius voluptates atque nihil voluptatem enim in tempore minima sit ad mollitia commodi minus.</p>
 
 
@@ -48,17 +120,20 @@
       </div>
 
       <div class="col-lg-8">
-      <form action="{{ route('enquiry') }}" method="POST" role="form" class="php-email-form">
+        <form action="{{ route('enquiry') }}" method="POST" role="form" class="php-email-form">
           <div class="row">
             <div class="col-md-6 form-group">
               <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
             </div>
-            <div class="col-md-6 form-group mt-3 mt-md-0">
-              <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+            <div class="col-md-6 form-group">
+              <input type="text" name="age" class="form-control" id="age" placeholder="Your Age" required>
             </div>
-          </div>
-          <div class="form-group mt-3">
-            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+            <div class="col-md-6 form-group">
+              <input type="text" name="city" class="form-control" id="city" placeholder="Preference City">
+            </div>
+            <div class="col-md-6 form-group">
+              <input type="text" name="mobile" class="form-control" id="mobile" placeholder="Your Mobile" required>
+            </div>
           </div>
           <div class="form-group mt-3">
             <textarea class="form-control" name="message" placeholder="Message" required></textarea>
